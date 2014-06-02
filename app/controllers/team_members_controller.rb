2054b546567e5d91125bd24d9e7fb5341ca89501
Anthony Good <good.anthony@gmail.com>
2014-06-02 17:21:15 +0100

    cleans seeds and team_members_controller

--- app/controllers/team_members_controller.rb
+++ app/controllers/team_members_controller.rb
@@ -2,122 +2,112 @@ class TeamMembersController < ApplicationController
   include BCrypt
   
   def index
-	unless logged_in?
+    unless logged_in?
       flash[:notice] = "You need to sign in to see the Quipper team."
-	  return redirect_to "/sign-in"
-	end
+    return redirect_to "/sign-in"
+  end
     
-	@team = TeamMember.includes(:wins, :losses).sort { |a,b| a.name <=> b.name }
-	#@biggest_winner = TeamMember.with_most(@team, :wins)
-	#@biggest_loser = TeamMember.with_most(@team, :losses)
-	
-	@biggest_winner = TeamMember.biggest_winner(@team)
-	@biggest_loser = TeamMember.biggest_loser(@team)
-	
-	#@best_streaker = TeamMember.biggest_streaker(@team, :wins)
-	#@worst_streaker = TeamMember.biggest_streaker(@team, :losses)
-	
-	@best_streaker = get_best_streaker
-	@best_streak = @best_streaker.streaks_light
-	@worst_streaker = get_worst_streaker
-	@worst_streak = @worst_streaker.streaks_light(:losses)
-	
-	
-	#render :nothing => true
-	
+  @team = TeamMember.includes(:wins, :losses).sort { |a,b| a.name <=> b.name }
+  
+  @biggest_winner = TeamMember.biggest_winner(@team)
+  @biggest_loser = TeamMember.biggest_loser(@team)
+  
+  @best_streaker = get_best_streaker
+  @best_streak = @best_streaker.streaks_light
+  @worst_streaker = get_worst_streaker
+  @worst_streak = @worst_streaker.streaks_light(:losses)
+  
   end
   
   def show
-	
-  	unless logged_in?
+    unless logged_in?
       flash[:notice] = "You need to sign in to view someone's profile."
-	  return redirect_to "/sign-in"
-	end
-	
-	@team_member = TeamMember.find( params[:id])
-	@heading = appellation
-	
-	@wins = @team_member.wins.includes(:question).all
-	@losses = @team_member.losses.includes(:question).all
-	
-	@last_victory = @wins.last
-	@last_defeat = @losses.last
-	
-	
-	@win_streak = @team_member.streaks_light(:wins)
-	@loss_streak = @team_member.streaks_light(:losses)
-	
-
-	
-	if @team_member.most_likely_to 
-	  @likeliest = format_likelihoods( @team_member.most_likely_to )
-	end
-	
-	if @team_member.least_likely_to
-	  @unlikeliest = format_likelihoods( @team_member.least_likely_to ).reverse
-	end
+      return redirect_to "/sign-in"
+    end
+  
+    @team_member = TeamMember.find( params[:id])
+    @heading = appellation
+    
+    @wins = @team_member.wins.includes(:question).all
+    @losses = @team_member.losses.includes(:question).all
+    
+    @last_victory = @wins.last
+    @last_defeat = @losses.last
+    
+    
+    @win_streak = @team_member.streaks_light(:wins)
+    @loss_streak = @team_member.streaks_light(:losses)
+    
+    if @team_member.most_likely_to 
+      @likeliest = format_likelihoods( @team_member.most_likely_to )
+    end
+    
+    if @team_member.least_likely_to
+      @unlikeliest = format_likelihoods( @team_member.least_likely_to ).reverse
+    end
   end
   
-  def sign_in	
+  def sign_in  
     # redirect if the user is already signed in
-	return redirect_to team_member_path(session[:team_member_id]) if logged_in?
-	
-	render 'sign_in'
-
+    return redirect_to team_member_path(session[:team_member_id]) if logged_in?
+  
+    render 'sign_in'
   end
   
   def contests
     @team_member = TeamMember.find( params[:id] )
-	@contests = @team_member.contests
-	@heading = "Contests"
+    @contests = @team_member.contests
+    @heading = "Contests"
   end
   
 
   def check_user
-  	email = params[:email] << "@quipper.com"
-	@user = TeamMember.find_by_email( email )
-	submitted_password = params[:password]
-	
-	if submitted_password.empty?
-	  flash[:alert] = "Enter a password, you crazy person."
-	  return redirect_to :back
-	end
-	
-	# if the entered email address matches a user ...
-	if @user
+    email = params[:email] << "@quipper.com"
+    @user = TeamMember.find_by_email( email )
+    submitted_password = params[:password]
+  
+    if submitted_password.empty?
+      flash[:alert] = "Enter a password, you crazy person."
+      return redirect_to :back
+    end
+  
+    # if the entered email address matches a user ...
+    if @user
       # check if user has already registered (password exists)
       if @user.password_hash
-	    # check their password is correct
-		if Password.new( @user.password_hash) == submitted_password 
-		
+        # check their password is correct
+        if Password.new( @user.password_hash) == submitted_password 
+      
           # flash[:notice] = "You've logged in as #{@user.name}"
-		  session[:team_member_id] = @user.id
-		  return redirect_to '/contests/new'
-		else
-		  flash[:alert] = "Email is registered, but not with that password."
-		  return redirect_to :back
-		end
-      else
-	    # team_member found, but not registered yet
-		# register their account
-		hashed_password = Password.create( submitted_password )
-		@user.update_attribute( :password_hash, hashed_password )
-		
-		flash[:notice] = "Account created for #{@user.name}!"
-		session[:team_member_id] = @user.id
-		
-		redirect_to '/contests/new'
-      end		
-	else 
-	  flash[:alert] = "No user was found with that email address. Are you sure you work for Quipper?"
-	  redirect_to "/sign-in"
-	end
-  
+          session[:team_member_id] = @user.id
+          return redirect_to '/contests/new'
+          
+      else      
+        flash[:alert] = "Email is registered, but not with that password."
+        return redirect_to :back        
+      end
+      
+        else
+        # team_member found, but not registered yet
+        # register their account
+        hashed_password = Password.create( submitted_password )
+        @user.update_attribute( :password_hash, hashed_password )
+      
+        flash[:notice] = "Account created for #{@user.name}!"
+        session[:team_member_id] = @user.id
+      
+        redirect_to '/contests/new'
+        end 
+        
+    else 
+      flash[:alert] = "No user was found with that email address. Are you sure you work for Quipper?"
+      redirect_to "/sign-in"
+    end 
   end
 
   def logout
     session[:team_member_id] = nil
-	redirect_to '/sign-in'
+    redirect_to '/sign-in'
   end
 
   def get_best_streaker
@@ -133,17 +123,26 @@ class TeamMembersController < ApplicationController
   
 
   def appellation
-    ["Quipper Colleague","Quiz Stalwart","Education Apostle","Distributor of Wisdom","Courier of Knowledge","Knowledge Expert","Thinker of Big Ideas","Quipper Footsoldier", "Learner", "Professor of Truth and Light", "Quasher of Ignorance"].sample(1)[0]
+    ["Quipper Colleague",
+    "Quiz Stalwart",
+    "Education Apostle",
+    "Distributor of Wisdom",
+    "Courier of Knowledge",
+    "Knowledge Expert",
+    "Thinker of Big Ideas",
+    "Quipper Footsoldier", 
+    "Learner", 
+    "Professor of Truth and Light", 
+    "Quasher of Ignorance"].sample(1)[0]
   end
   
   # receives a 2D array, should return pretty 1D array
   def format_likelihoods(arr)
     pretty = arr.map do |question, likelihood|
-	  [question.sub("Who would ", "").sub("?","").humanize, likelihood]
-	end
-	pretty
-  end
-  
-  
+      [question.sub("Who would ", "").sub("?","").humanize, likelihood]
+    end
+    
+    pretty
+  end 
   
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,40 +7,40 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 names = %w[
-	Masayuki\ Watanabe
-	Anthony\ Good
-	Wis\ Amarasinghe
-	Masatomo\ Nakano
-	Seth\ Jeffery
-	Omar\ Sahyoun
-	Joseph\ Ganderson
-	Takuya\ Homma
-	Yosuke\ Arakaki
-	Kazuyuki\ Honda
-	Akifumi\ Yokoi
-	Yuki\ Miyauchi
-	Flavio\ Zanda
-	Roxana\ Rudareanu
-	Takeshi\ Ugajin
-	Kensuke\ Nagae
-	Kohei\ Hasegawa
-	Daisuke\ Fujimura
-	Takeshi\ Sasaki
-	Koutaro\ Chikuba
-	Yusuke\ Kaneko
-	Matthew\ Platts
-	Mai\ Ueno
-	Tomoko\ Matsukawa
-	Eri\ Nishihara
-	Akalpa\ Acharya
-	Ryan\ Guerrero
-	Yuta\ Funase
-	Keisuke\ Kawase
-	Bianca\ Esmero
-	Satoru\ Tanabe
-	Kati\ Vorobjova
-	Takeyoshi\ Mizusawa
-	Yuki\ Naotori
+  Masayuki\ Watanabe
+  Anthony\ Good
+  Wis\ Amarasinghe
+  Masatomo\ Nakano
+  Seth\ Jeffery
+  Omar\ Sahyoun
+  Joseph\ Ganderson
+  Takuya\ Homma
+  Yosuke\ Arakaki
+  Kazuyuki\ Honda
+  Akifumi\ Yokoi
+  Yuki\ Miyauchi
+  Flavio\ Zanda
+  Roxana\ Rudareanu
+  Takeshi\ Ugajin
+  Kensuke\ Nagae
+  Kohei\ Hasegawa
+  Daisuke\ Fujimura
+  Takeshi\ Sasaki
+  Koutaro\ Chikuba
+  Yusuke\ Kaneko
+  Matthew\ Platts
+  Mai\ Ueno
+  Tomoko\ Matsukawa
+  Eri\ Nishihara
+  Akalpa\ Acharya
+  Ryan\ Guerrero
+  Yuta\ Funase
+  Keisuke\ Kawase
+  Bianca\ Esmero
+  Satoru\ Tanabe
+  Kati\ Vorobjova
+  Takeyoshi\ Mizusawa
+  Yuki\ Naotori
 ]
 
 names.each do |person|
@@ -66,77 +66,77 @@ team_portraits.each do |image_name|
 end
 
 questions = [
-	#"Who would alienate themselves and others?",
-	#"Who would kill for love?",
-	"Who would win at ping pong?",
-	"Who would win at fussball?",
-	"Who would score a penalty?",
-	#"Who would start their own business?",
-	"Who would sell themselves to the highest bidder?",
-	"Who would sneeze without covering their mouth?",
-	#"Who would conduct their affairs in secret?",
-	#"Who would destroy something beautiful?",
-	#"Who would build a better house?",
-	#"Who would surprise themselves the most?",
-	#"Who would wish you a happy birthday?",
-	"Who would give money to a tramp?",
-	#"Who would speak loudly in quiet surroundings?",
-	#"Who would ponder life's hidden meaning?",
-	"Who would run a marathon?",
-	"Who would win in a battle to the death?",
-	"Who would get a tattoo?",
-	"Who would get inappropriately drunk?",
-	#"Who would wake up in unfamiliar surroundings?",
-	"Who would slay the dragon?",
-	#"Who would bite off more than they could chew?",
-	"Who would start a revolution?",
-	#"Who would vote Conservative?",
-	#"Who would vote Labour?",
-	"Who would blame others for their own farts?",
-	"Who would live fast and die young?",
-	#"Who would swear on their mother's life?",
-	#"Who would lie through their teeth?",
-	"Who would paint a crap picture?",
-	"Who would compose a reasonably good symphony?",
-	#"Who would delude themselves?",
-	"Who would travel the world?",
-	#"Who would unmask Batman?",
-	"Who would live amongst the animals?",
-	#"Who would bark in anger?",
-	"Who would bark in fear?",
-	"Who would best control their bladder?",
-	#"Who would defeat their inner demons?",
-	"Who would do better at maths?",
-	"Who would sing surprisingly well?",
-	"Who would be seduced by their own reflection?",
-	#"Who would travel the world and be disappointed by it?",
-	"Who would survive the apocalypse?",
-	"Who would shoot you if you turned into a zombie?",
-	#"Who would sign a pact with the devil?",
-	"Who would spend ages looking for their keys, only to find them in their pocket?",
-	"Who would react badly to losing their phone?",
-	#"Who would ask a silly question?",
-	#"Who would declare a bold truth?",
-	#"Who would give you the finger?",
-	"Who would mutter under their breath?",
-	"Who would brew their tea longest?",
-	"Who would take their coffee black?",
-	"Who would enjoy a trashy novel?",
-	"Who would listen to bad pop?",
-	"Who would go to an art gallery and actually enjoy it?",
-	#"Who would buy an expensive sandwich?",
-	#"Who would wear expensive clothes?",
-	"Who would pay for lunch?",
-	"Who would tip handsomely?",
-	#"Who would decide the matter outright?",
-	#"Who would laugh in the other's face?",
-	#"Who would be content in themselves?",
-	"Who would turn the other cheek?",
-	#"Who would fight with grave violence?",
-	#"Who would go berserk?",
-	#"Who would triumph in adversity?",
-	#"Who would own the means of production?",
-	#"Who would play the piano?",
+  #"Who would alienate themselves and others?",
+  #"Who would kill for love?",
+  "Who would win at ping pong?",
+  "Who would win at fussball?",
+  "Who would score a penalty?",
+  #"Who would start their own business?",
+  "Who would sell themselves to the highest bidder?",
+  "Who would sneeze without covering their mouth?",
+  #"Who would conduct their affairs in secret?",
+  #"Who would destroy something beautiful?",
+  #"Who would build a better house?",
+  #"Who would surprise themselves the most?",
+  #"Who would wish you a happy birthday?",
+  "Who would give money to a tramp?",
+  #"Who would speak loudly in quiet surroundings?",
+  #"Who would ponder life's hidden meaning?",
+  "Who would run a marathon?",
+  "Who would win in a battle to the death?",
+  "Who would get a tattoo?",
+  "Who would get inappropriately drunk?",
+  #"Who would wake up in unfamiliar surroundings?",
+  "Who would slay the dragon?",
+  #"Who would bite off more than they could chew?",
+  "Who would start a revolution?",
+  #"Who would vote Conservative?",
+  #"Who would vote Labour?",
+  "Who would blame others for their own farts?",
+  "Who would live fast and die young?",
+  #"Who would swear on their mother's life?",
+  #"Who would lie through their teeth?",
+  "Who would paint a crap picture?",
+  "Who would compose a reasonably good symphony?",
+  #"Who would delude themselves?",
+  "Who would travel the world?",
+  #"Who would unmask Batman?",
+  "Who would live amongst the animals?",
+  #"Who would bark in anger?",
+  "Who would bark in fear?",
+  "Who would best control their bladder?",
+  #"Who would defeat their inner demons?",
+  "Who would do better at maths?",
+  "Who would sing surprisingly well?",
+  "Who would be seduced by their own reflection?",
+  #"Who would travel the world and be disappointed by it?",
+  "Who would survive the apocalypse?",
+  "Who would shoot you if you turned into a zombie?",
+  #"Who would sign a pact with the devil?",
+  "Who would spend ages looking for their keys, only to find them in their pocket?",
+  "Who would react badly to losing their phone?",
+  #"Who would ask a silly question?",
+  #"Who would declare a bold truth?",
+  #"Who would give you the finger?",
+  "Who would mutter under their breath?",
+  "Who would brew their tea longest?",
+  "Who would take their coffee black?",
+  "Who would enjoy a trashy novel?",
+  "Who would listen to bad pop?",
+  "Who would go to an art gallery and actually enjoy it?",
+  #"Who would buy an expensive sandwich?",
+  #"Who would wear expensive clothes?",
+  "Who would pay for lunch?",
+  "Who would tip handsomely?",
+  #"Who would decide the matter outright?",
+  #"Who would laugh in the other's face?",
+  #"Who would be content in themselves?",
+  "Who would turn the other cheek?",
+  #"Who would fight with grave violence?",
+  #"Who would go berserk?",
+  #"Who would triumph in adversity?",
+  #"Who would own the means of production?",
+  #"Who would play the piano?",
 ]
 
 questions.each do |q|
